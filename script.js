cfba9efeb6ad068f1e2b96217ddedadd826c88bd
Shenor <dreik7570000@gmail.com>
2019-03-26 11:10:43 +0300

    Create Timer

--- script.js
+++ script.js
@@ -15,23 +15,67 @@ window.addEventListener('DOMContentLoaded', function () {
     hideTabContent(1);
     console.log(info);
 
-function showTabContent(b) {
-    if (tabContent[b].classList.contains('hide')) {
-        tabContent[b].classList.remove('hide');
-        tabContent[b].classList.add('show');
-    }
-}
-
-info.addEventListener('click', function () {
-    let target = event.target;
-    if (target && target.classList.contains('info-header-tab')) {
-        for (let i = 0; i < tab.length; i++) {
-            if (target == tab[i]) {
-                hideTabContent(0);
-                showTabContent(i);
-                break;
-            }
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
         }
     }
-});
+
+    info.addEventListener('click', function () {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+
+    //Timer
+
+    let deadline = '2019-3-27';
+
+    function getTimeRemaning(entime){
+        let t = Date.parse(entime) - Date.parse(new Date()),
+        seconds = new Date(t).getSeconds(),
+        minutes = new Date(t).getMinutes(),
+        hours = new Date(t).getHours();
+
+        return {
+            'total': t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+
+    function setClock(id, entime){
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(update, 1000);
+
+            function update(){
+                let t = getTimeRemaning(entime);
+
+                t.minutes.toString().length == 1? hours.textContent = '0' + t.hours : hours.textContent = t.hours;
+                t.minutes.toString().length == 1? minutes.textContent = '0' + t.minutes : minutes.textContent = t.minutes;
+                t.seconds.toString().length == 1? seconds.textContent = '0' + t.seconds : seconds.textContent = t.seconds;
+
+                if (t.total <= 0){
+                    clearInterval(timeInterval);
+                    hours.textContent = '00';
+                    minutes.textContent = '00';
+                    seconds.textContent = '00';
+                }
+            }
+
+    }
+    setClock('timer', deadline);
 });
