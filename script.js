5091b3dbcc9a3d5a19e3ca5716eb8f22867b4cbb
Shenor <dreik7570000@gmail.com>
2019-03-26 13:20:11 +0300

    Добавлние модалього окна и работа с this

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ window.addEventListener('DOMContentLoaded', function () {
             function update(){
                 let t = getTimeRemaning(entime);
 
-                t.minutes.toString().length == 1? hours.textContent = '0' + t.hours : hours.textContent = t.hours;
+                t.hours.toString().length == 1? hours.textContent = '0' + t.hours : hours.textContent = t.hours;
                 t.minutes.toString().length == 1? minutes.textContent = '0' + t.minutes : minutes.textContent = t.minutes;
                 t.seconds.toString().length == 1? seconds.textContent = '0' + t.seconds : seconds.textContent = t.seconds;
 
@@ -78,4 +78,33 @@ window.addEventListener('DOMContentLoaded', function () {
 
     }
     setClock('timer', deadline);
+
+    // Modal
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close'),
+        descriptBtn = document.querySelectorAll('.description-btn');
+
+        more.addEventListener('click', function(){
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        });
+
+        close.addEventListener('click', function(){
+            overlay.style.display = 'none';
+            more.classList.remove('more-splash');
+            document.body.style.overflow = '';
+        });
+
+        //Вызов функции модального окна для каждой функции
+        
+        descriptBtn.forEach(function(item){
+            item.addEventListener('click', function(){
+                overlay.style.display = 'block';
+                this.classList.add('more-splash');
+                document.body.style.overflow = 'hidden';
+            });
+        });
+       
 });
