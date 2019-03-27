d11cdb5a4bc1a12824a4cc38811070b4a84fa779
Shenor <dreik7570000@gmail.com>
2019-03-27 15:11:07 +0300

    Создание слайдера

--- script.js
+++ script.js
@@ -151,7 +151,8 @@ window.addEventListener('DOMContentLoaded', function () {
 
              request.send(obj);   
              
-             //Обработка событий ответа сервера
+             //Обработка событий ответа
+             
              request.addEventListener('readystatechange', function(){
                  if(request.readyState < 4 ){
                     statusMessage.innerHTML = message.loading;
@@ -163,5 +164,53 @@ window.addEventListener('DOMContentLoaded', function () {
              });
         });
 
+        //slider
+        let slideIndex = 1,
+            slides = document.querySelectorAll('.slider-item'),
+            prev = document.querySelector('.prev'),
+            next = document.querySelector('.next'),
+            dotsWrap = document.querySelector('.slider-dots'),
+            dots = document.querySelectorAll('.dot');
 
+            showSlides(slideIndex);
+
+            function showSlides(n){
+                if(n > slides.length){
+                    slideIndex = 1;
+                }
+                if(n < 1){
+                    slideIndex = slides.length
+                }
+    
+                slides.forEach((item) => item.style.display = 'none');
+                dots.forEach((item) => item.classList.remove('dot-active'));
+
+                slides[slideIndex - 1].style.display = 'block';
+                dots[slideIndex - 1].classList.add('dot-active');
+            };
+            
+            function plusSlider(n){
+                showSlides(slideIndex += n);
+            };
+
+            function currentSlider(n){
+                showSlides(slideIndex = n);
+            };
+            
+
+            prev.addEventListener('click', function(){
+                plusSlider(-1);
+            });
+
+            next.addEventListener('click', function(){
+                plusSlider(1);
+            });
+
+            dotsWrap.addEventListener('click', function(event){
+                for(let i = 0; i < dots.length + 1; i++){
+                    if (event.target.classList.contains('dot') && event.target == dots[i - 1]){
+                        currentSlider(i);
+                    } 
+                }
+            });
 });
