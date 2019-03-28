39a22548c66bc9d7a95dd952893cbc8e3a566811
Shenor <dreik7570000@gmail.com>
2019-03-28 12:42:48 +0300

    @@@@

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     }
     hideTabContent(1);
-    console.log(info);
+    
 
     function showTabContent(b) {
         if (tabContent[b].classList.contains('hide')) {
@@ -212,5 +212,43 @@ window.addEventListener('DOMContentLoaded', function () {
                         currentSlider(i);
                     } 
                 }
+                
             });
+
+            //Calculator
+            let person = document.querySelectorAll('.counter-block-input')[0],
+            day = document.querySelectorAll('.counter-block-input')[1],
+            place = document.querySelector('#select')[0],
+            totalValue = document.querySelector('#total'),
+            personSum = 0,
+            daySum = 0,
+            total = 0;
+
+            totalValue.textContent = 0;
+
+            person.addEventListener('input', function(){
+                personSum = +this.value;
+               
+                if(day.value == '' || personSum == '' || personSum == 0){
+                    totalValue.textContent = 0;
+                    total = 0;
+                }else{
+                    totalValue.textContent =  total;
+                    total = (daySum + personSum) * 4000;
+                }
+            });
+
+            day.addEventListener('input', function(){
+                daySum = +this.value;
+                //условие слайдера
+                if(person.value == '' || daySum == 0 || daySum == ''){
+                    totalValue.textContent = 0;
+                    total = 0;
+                }else{
+                    total = (daySum + personSum) * 4000;
+                    totalValue.textContent =  total;
+                }
+            });
+            
+            
 });
